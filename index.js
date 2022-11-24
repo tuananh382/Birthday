52f642adb0e00ed4b8d3b055c1e221ce8e5411b0
Huynh Tuan Anh <20520383@gm.uit.edu.vn>
2022-11-24 16:18:55 +0700

    2

--- index.js
+++ index.js
@@ -19,11 +19,12 @@
     $('.fire').click(function(){
         $(this).removeClass('fire');
         audio.play();
+        Draw();
     })
 
     console.clear();
 
-let particles = [];
+let particless = [];
 const colors = ["#eb6383","#fa9191","#ffe9c5","#b4f2e1"];
 function pop () {
   for (let i = 0; i < 150; i++) {
@@ -35,7 +36,7 @@ function pop () {
       y: Math.random() * -20 - 15
     };
     p.mass = Math.random() * 0.2 + 0.8;
-    particles.push(p);
+    particless.push(p);
     p.style.transform = `translate(${p.x}px, ${p.y}px)`;
     const size = Math.random() * 15 + 5;
     p.style.width = size + 'px';
@@ -46,8 +47,8 @@ function pop () {
 }
 
 function render () {
-  for (let i = particles.length - 1; i--; i > -1) {
-    const p = particles[i];
+  for (let i = particless.length - 1; i--; i > -1) {
+    const p = particless[i];
     p.style.transform = `translate3d(${p.x}px, ${p.y}px, 1px)`;
     
     p.x += p.vel.x;
@@ -56,7 +57,7 @@ function render () {
     p.vel.y += (0.5 * p.mass);
     if (p.y > (window.innerHeight * 2)) {
       p.remove();
-      particles.splice(i, 1);
+      particless.splice(i, 1);
     }
   }
   requestAnimationFrame(render);
@@ -64,4 +65,111 @@ function render () {
 pop();
 window.setTimeout(render, 700);
 window.addEventListener('click', pop);
+
+
+let W = window.innerWidth;
+let H = window.innerHeight;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const maxConfettis = 150;
+const particles = [];
+
+const possibleColors = [
+  "DodgerBlue",
+  "OliveDrab",
+  "Gold",
+  "Pink",
+  "SlateBlue",
+  "LightBlue",
+  "Gold",
+  "Violet",
+  "PaleGreen",
+  "SteelBlue",
+  "SandyBrown",
+  "Chocolate",
+  "Crimson"
+];
+
+function randomFromTo(from, to) {
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+function confettiParticle() {
+  this.x = Math.random() * W; // x
+  this.y = Math.random() * H - H; // y
+  this.r = randomFromTo(11, 33); // radius
+  this.d = Math.random() * maxConfettis + 11;
+  this.color =
+    possibleColors[Math.floor(Math.random() * possibleColors.length)];
+  this.tilt = Math.floor(Math.random() * 33) - 11;
+  this.tiltAngleIncremental = Math.random() * 0.07 + 0.05;
+  this.tiltAngle = 0;
+
+  this.draw = function() {
+    context.beginPath();
+    context.lineWidth = this.r / 2;
+    context.strokeStyle = this.color;
+    context.moveTo(this.x + this.tilt + this.r / 3, this.y);
+    context.lineTo(this.x + this.tilt, this.y + this.tilt + this.r / 5);
+    return context.stroke();
+  };
+}
+
+function Draw() {
+  const results = [];
+
+  // Magical recursive functional love
+  requestAnimationFrame(Draw);
+
+  context.clearRect(0, 0, W, window.innerHeight);
+
+  for (var i = 0; i < maxConfettis; i++) {
+    results.push(particles[i].draw());
+  }
+
+  let particle = {};
+  let remainingFlakes = 0;
+  for (var i = 0; i < maxConfettis; i++) {
+    particle = particles[i];
+
+    particle.tiltAngle += particle.tiltAngleIncremental;
+    particle.y += (Math.cos(particle.d) + 3 + particle.r / 2) / 2;
+    particle.tilt = Math.sin(particle.tiltAngle - i / 3) * 15;
+
+    if (particle.y <= H) remainingFlakes++;
+
+    // If a confetti has fluttered out of view,
+    // bring it back to above the viewport and let if re-fall.
+    if (particle.x > W + 30 || particle.x < -30 || particle.y > H) {
+      particle.x = Math.random() * W;
+      particle.y = -30;
+      particle.tilt = Math.floor(Math.random() * 10) - 20;
+    }
+  }
+
+  return results;
+}
+
+window.addEventListener(
+  "resize",
+  function() {
+    W = window.innerWidth;
+    H = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  },
+  false
+);
+
+// Push new confetti objects to `particles[]`
+for (var i = 0; i < maxConfettis; i++) {
+  particles.push(new confettiParticle());
+}
+
+// Initialize
+canvas.width = W;
+canvas.height = H;
+// Draw();
+
+
   });
